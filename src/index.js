ec7e42a31048501ed4cf89f8e8ce3240ff79184a
Long Viet Tran <longtv110998@gmail.com>
2023-09-15 14:40:34 +0700

    update method cors

--- src/index.js
+++ src/index.js
@@ -4,21 +4,16 @@ const router = jsonServer.router("server.json");
 const middlewares = jsonServer.defaults();
 const auth = require("json-server-auth");
 const cors = require("cors");
-const express = require("express");
 
-const app = express();
 server.db = router.db;
 server.use(auth);
-app.use(cors({ origin: ["http://localhost:5173"] }));
-const corsOpts = {
-  origin: "*",
 
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-
-  allowedHeaders: ["Content-Type"],
-};
-
-app.use(cors(corsOpts));
+server.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["POST", "GET", "OPTIONS"],
+  })
+);
 server.use(router);
 
 const port = process.env.PORT || 3000;
